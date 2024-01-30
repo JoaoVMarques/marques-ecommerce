2a3598d529ac1ec965570e125090fa9707f3c545
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2024-01-30 17:32:48 -0300

    Adicionando mais uma qualidade ao site

--- src/pages/infoPage/components/introObject.ts
+++ src/pages/infoPage/components/introObject.ts
@@ -1,16 +1,22 @@
 import clockIcon from '../../../imgs/svgs/clock.svg';
 import supportIcon from '../../../imgs/svgs/support.svg';
+import cartIcon from '../../../imgs/svgs/cart.svg';
 
 export const clock = {
   icon: clockIcon,
-  description: 'sua loja aberta 24 horas por dia, toda semana',
+  description: 'Sua loja aberta 24 horas por dia, toda semana',
 };
 
 export const support = {
   icon: supportIcon,
-  description: 'suporte, rapido e pratico a todo momento',
+  description: 'Suporte, rapido e pratico a todo momento',
 };
 
-const allObjects = [clock, support];
+export const cart = {
+  icon: cartIcon,
+  description: 'Compre e venda tudo com facilidade, e velocidade',
+};
+
+const allObjects = [clock, support, cart];
 
 export default allObjects;
