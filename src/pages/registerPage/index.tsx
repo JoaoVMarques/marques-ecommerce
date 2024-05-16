81a87a318b9e5437082d3bb1dba3327b5c5ac858
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2024-05-16 00:33:29 -0300

    criando de registro para cada plano

---
+++ src/pages/registerPage/index.tsx
@@ -0,0 +1,9 @@
+function RegisterPage() {
+  return (
+    <>
+      <h1>crie sua conta agora</h1>
+    </>
+  );
+}
+
+export default RegisterPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import InfoPage from '../pages/infoPage';
 import PlansPage from '../pages/plansPage';
+import RegisterPage from '../pages/registerPage';
+
+const plansRoutes = [
+  'cobalt',
+  'silver',
+  'gold',
+  'diamond',
+  'bronze',
+];
 
 function Router() {
   return (
     <Routes>
       <Route path='/' element={<InfoPage />} />
       <Route path='/plans' element={<PlansPage />} />
+      <Route>
+        {plansRoutes.map((path, index) => (
+          <Route path={`/subscribe/${path}-plan`} element={
+            <RegisterPage />
+          }
+          key={index}
+          />
+        ))}
+      </Route>
     </Routes>
   );
 }
