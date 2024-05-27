933e46d433dddf5ce92aaed8719ea0bc9d9b4a5e
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2024-05-27 05:12:09 -0300

    identando e adicionando background ao container

--- src/pages/registerPage/index.tsx
+++ src/pages/registerPage/index.tsx
@@ -1,7 +1,22 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import registerLogo from '../../imgs/register-logo.png';
+
 function RegisterPage() {
   return (
     <>
-      <h1>crie sua conta agora</h1>
+      <Container className='d-flex justify-content-center'>
+        <Row>
+          <Col>
+            <img
+              className='white-secondary-backgroud w-100 p-5'
+              src={registerLogo}
+              alt="logo de registro ecommerce" />
+          </Col>
+          <Col>
+            <h2>asd</h2>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
